b4e90337b7a3f5efe76c430264530dd0bd99f1b2
Joche3023 <j.zumbado.1@estudiantec.cr>
2026-05-07 22:45:33 -0600

    feat: pasarlo a TypeScript

--- src/controllers/health.controller.js
+++
@@ -1,7 +0,0 @@
-const { healthCheck } = require('../services/health.service');
-
-const getHealth = (req, res) => {
-  res.status(200).json(healthCheck());
-};
-
-module.exports = { getHealth };
--- src/services/health.service.js
+++
@@ -1,6 +0,0 @@
-const healthCheck = () => ({
-  success: true,
-  message: 'API is running',
-});
-
-module.exports = { healthCheck };
