3dca1ea24f89ae68b80f07f9968de29d176ebc0e
Joche3023 <j.zumbado.1@estudiantec.cr>
2026-05-08 11:56:12 -0600

    feat: añadir config SSL

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -4,4 +4,7 @@ import { loadEnv } from '../config/env';
 
 loadEnv();
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes('render.com') ? { rejectUnauthorized: false } : false,
+});
